350871406a6fd3bda5d4d3b567d6583882c9cd2f
Ashish Solao <asolao@netrixllc.com>
2019-06-22 19:24:25 +0530

    Added shouldComponentUpdate check for order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingSummary = Object.keys(props.ingredients)
-        .map(ingKey => {
-            return (
-                <li key={ingKey}>
-                    <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
-                </li>
-            );
-        });
-
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with following ingredients:</p>
-            <ul>
-                {ingSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout</p>
-            <Button clicked={props.purchaseCanceled} btnType='Danger'>CANCEL</Button>
-            <Button clicked={props.purchaseContinued} btnType='Success'>CONTINUE</Button>
-        </Aux>
-    );
+class OrderSummary extends Component {
+    render () {
+        const ingSummary = Object.keys(this.props.ingredients)
+            .map(ingKey => {
+                return (
+                    <li key={ingKey}>
+                        <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]}
+                    </li>
+                );
+            });
+            
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with following ingredients:</p>
+                <ul>
+                    {ingSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout</p>
+                <Button clicked={this.props.purchaseCanceled} btnType='Danger'>CANCEL</Button>
+                <Button clicked={this.props.purchaseContinued} btnType='Success'>CONTINUE</Button>
+            </Aux>
+        )
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
