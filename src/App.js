b26574c1fc4afbcd2aaabd2f4749516a37dcafb8
Ashish Solao <asolao@netrixllc.com>
2019-07-18 21:20:49 +0530

    Implemented Lazy loading of components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Layout from './hoc/Layout/Layout';
-import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import {connect} from 'react-redux';
-import * as actions from './store/actions/'
+import * as actions from './store/actions/';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+const asyncCheckout = asyncComponent(() => {
+	return import('./containers/Checkout/Checkout');
+});
+
+const asyncAuth = asyncComponent(() => {
+	return import('./containers/Auth/Auth');
+});
+
+const asyncOrders = asyncComponent(() => {
+	return import('./containers/Orders/Orders');
+});
+
 class App extends Component {
 
 	componentDidMount () {
@@ -17,7 +28,7 @@ class App extends Component {
   	render() {
 		let routes = (
 			<Switch>
-				<Route path="/auth" component={Auth} />
+				<Route path="/auth" component={asyncAuth} />
 				<Route path="/" exact component={BurgerBuilder} />
 				<Redirect to="/" />
 			</Switch>
@@ -26,10 +37,10 @@ class App extends Component {
 		if(this.props.isAuthenticated) {
 			routes = (
 				<Switch>
-					<Route path="/checkout" component={Checkout} />
-					<Route path="/orders" component={Orders} />
+					<Route path="/checkout" component={asyncCheckout} />
+					<Route path="/orders" component={asyncOrders} />
 					<Route path="/logout" component={Logout} />
-					<Route path="/auth" component={Auth} />
+					<Route path="/auth" component={asyncAuth} />
 					<Route path="/" exact component={BurgerBuilder} />
 					<Redirect to="/" />
 				</Switch>
